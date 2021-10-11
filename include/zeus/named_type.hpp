36b24ffd66257ee615d49f13741b19d9b5f77d2f
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2021-10-11 13:11:00 -0700

    [brief] Adds a strong typing header system.
    [detailed]
    - Seems like a good idea to have a setup for this around, in case it is
      ever needed.
    - Adds tests for the strong typing (also has the nice side-effect of
      testing the CRTP header).

---
+++ include/zeus/named_type.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "crtp.hpp"
+
+// This header defines a strong-typing interface along with a set of common
+// operations taken from FluentC++
+// https://www.fluentcpp.com/2017/05/23/strong-types-inheriting-functionalities-from-underlying/
+
+namespace zeus
+{
+    template<typename T, typename Parameter, template<typename> class... Skills>
+    class NamedType : public Skills<NamedType<T, Parameter, Skills...>>...
+    {
+    public:
+        explicit NamedType(T val) : m_value{val}
+        {}
+
+        T& get()
+        {
+            return m_value;
+        }
+
+        T const& get() const
+        {
+            return m_value;
+        }
+
+    private:
+        T m_value;
+    };
+
+    template<typename T>
+    struct Addable : CRTPBase<T, Addable>
+    {
+        T& operator+=(T const& other)
+        {
+            this->self().get() += other.get();
+            return this->self();
+        }
+
+        T operator+(T const& other) const
+        {
+            return T{this->self().get() + other.get()};
+        }
+    };
+
+    template<typename T>
+    struct Subtractable : CRTPBase<T, Subtractable>
+    {
+        T& operator-=(T const& other)
+        {
+            this->self().get() -= other.get();
+            return this->self();
+        }
+
+        T operator-(T const& other) const
+        {
+            return T{this->self().get() - other.get()};
+        }
+    };
+
+    template<typename T>
+    struct Multiplicable : CRTPBase<T, Multiplicable>
+    {
+        T& operator*=(T const& other)
+        {
+            this->self().get() *= other.get();
+            return this->self();
+        }
+
+        T operator*(T const& other) const
+        {
+            return T{this->self().get() * other.get()};
+        }
+    };
+
+    template<typename T>
+    struct Divisible : CRTPBase<T, Divisible>
+    {
+        T& operator/=(T const& other)
+        {
+            this->self().get() /= other.get();
+            return this->self();
+        }
+
+        T operator/(T const& other) const
+        {
+            return T{this->self().get() / other.get()};
+        }
+    };
+} // namespace zeus
---
+++ test/named_type_test.cpp
@@ -0,0 +1,103 @@
+#include <zeus/named_type.hpp>
+
+#include <catch2/catch.hpp>
+
+#define MAKE_TEST_TYPE(TypeName, BaseType) \
+    using TypeName = zeus::NamedType<BaseType, struct BaseType##_param>
+
+#define MAKE_TEST_TYPE_V(TypeName, BaseType, ...) \
+    using TypeName =                              \
+        zeus::NamedType<BaseType, struct BaseType##_param, __VA_ARGS__>
+
+TEMPLATE_TEST_CASE("[NamedType] - get", "[zeus]", int, double)
+{
+    MAKE_TEST_TYPE(Type, TestType);
+
+    SECTION("Non-const object")
+    {
+        Type t{1};
+        REQUIRE(t.get() == 1);
+    }
+
+    SECTION("Const object")
+    {
+        const Type t{1};
+        REQUIRE(t.get() == 1);
+    }
+}
+
+TEMPLATE_TEST_CASE("[NamedType] - Addable", "[zeus]", int, double)
+{
+    MAKE_TEST_TYPE_V(Type, TestType, zeus::Addable);
+
+    Type t1{1}, t2{2};
+
+    SECTION("Operator +=")
+    {
+        t1 += t2;
+        REQUIRE(t1.get() == 3);
+    }
+
+    SECTION("Operator +")
+    {
+        auto t3 = t1 + t2;
+        REQUIRE(t3.get() == 3);
+    }
+}
+
+TEMPLATE_TEST_CASE("[NamedType] - Subtractable", "[zeus]", int, double)
+{
+    MAKE_TEST_TYPE_V(Type, TestType, zeus::Subtractable);
+
+    Type t1{1}, t2{2};
+
+    SECTION("Operator -=")
+    {
+        t2 -= t1;
+        REQUIRE(t2.get() == 1);
+    }
+
+    SECTION("Operator -")
+    {
+        auto t3 = t2 - t1;
+        REQUIRE(t3.get() == 1);
+    }
+}
+
+TEMPLATE_TEST_CASE("[NamedType] - Multiplicable", "[zeus]", int, double)
+{
+    MAKE_TEST_TYPE_V(Type, TestType, zeus::Multiplicable);
+
+    Type t1{2}, t2{3};
+
+    SECTION("Operator *=")
+    {
+        t1 *= t2;
+        REQUIRE(t1.get() == 6);
+    }
+
+    SECTION("Operator *")
+    {
+        auto t3 = t1 * t2;
+        REQUIRE(t3.get() == 6);
+    }
+}
+
+TEMPLATE_TEST_CASE("[NamedType] - Divisible", "[zeus]", int, double)
+{
+    MAKE_TEST_TYPE_V(Type, TestType, zeus::Divisible);
+
+    Type t1{4}, t2{2};
+
+    SECTION("Operator /=")
+    {
+        t1 /= t2;
+        REQUIRE(t1.get() == 2);
+    }
+
+    SECTION("Operator /")
+    {
+        auto t3 = t1 / t2;
+        REQUIRE(t3.get() == 2);
+    }
+}
