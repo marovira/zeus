eba56fc0580cdf06189b3ffca0170090b0d0883c
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2022-01-23 12:12:40 -0800

    [brief] Adds splitting to Timer.
    [detailed]
    - Should solve the problem where we need multiple timers to time
      sub-sections of something while keeping track of global time.

--- include/zeus/timer.hpp
+++ include/zeus/timer.hpp
@@ -8,26 +8,46 @@ namespace zeus
     class Timer
     {
     public:
-        constexpr Timer() = default;
-
         constexpr void start()
         {
             m_begin = Res::now();
+            m_split = m_begin;
         }
 
         constexpr void reset()
         {
-            m_begin = std::chrono::time_point<Res>();
+            m_begin = {};
+            m_split = {};
         }
 
         constexpr T elapsed() const
         {
-            return std::chrono::duration_cast<second>(Res::now() - m_begin)
-                .count();
+            return time_diff(m_begin);
+        }
+
+        constexpr T split() const
+        {
+            if (m_split == TimePoint{})
+            {
+                return 0;
+            }
+
+            T diff  = time_diff(m_split);
+            m_split = Res::now();
+            return diff;
         }
 
     private:
-        using second = std::chrono::duration<T>;
-        std::chrono::time_point<Res> m_begin;
+        using Duration  = std::chrono::duration<T>;
+        using TimePoint = std::chrono::time_point<Res>;
+
+        constexpr T time_diff(TimePoint const& first) const
+        {
+            return std::chrono::duration_cast<Duration>(Res::now() - first)
+                .count();
+        }
+
+        TimePoint m_begin;
+        mutable TimePoint m_split;
     };
 } // namespace zeus
--- test/timer_test.cpp
+++ test/timer_test.cpp
@@ -15,3 +15,40 @@ TEST_CASE("[timer] - timer: elapsed", "[zeus]")
 
     REQUIRE(elapsed == 1);
 }
+
+TEST_CASE("[timer] - Timer: split", "[zeus]")
+{
+    using zeus::Timer;
+    using namespace std::chrono_literals;
+
+    Timer<int> t;
+
+    SECTION("Empty timer")
+    {
+        auto ret = t.split();
+
+        REQUIRE(ret == 0);
+    }
+
+    SECTION("Started timer")
+    {
+        t.start();
+        std::this_thread::sleep_for(1s);
+        auto split = t.split();
+        REQUIRE(split == 1);
+    }
+
+    SECTION("Multiple splits")
+    {
+        t.start();
+        std::this_thread::sleep_for(1s);
+        auto split = t.split();
+        REQUIRE(split == 1);
+
+        std::this_thread::sleep_for(1s);
+        split        = t.split();
+        auto elapsed = t.elapsed();
+        REQUIRE(split == 1);
+        REQUIRE(elapsed == 2);
+    }
+}
