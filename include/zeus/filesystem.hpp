02073c59719a99d2023bf8a57e57e99c2a7a5450
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2022-09-27 10:20:52 -0700

    [brief] Restores the filesystem header.
    [detailed]
    - Don't really want to deal with this right now.

--- include/zeus/filesystem.hpp
+++ include/zeus/filesystem.hpp
@@ -2,28 +2,19 @@
 
 #include "platform.hpp"
 
-#include <chrono>
 #include <fmt/printf.h>
-#include <string>
 
-// It seems that in GCC and Clang the implementation of filesystem is not complete. This
-// mainly affects get_file_last_write, so we're going to need to branch of a different
-// implementation for that function
 #if defined(ZEUS_PLATFORM_WINDOWS)
-#    include <filesystem>
-#else
-#    include <experimental/filesystem>
+#    define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 #endif
+#include <experimental/filesystem>
+#include <string>
 
 namespace zeus
 {
     inline std::time_t get_file_last_write(std::string filename)
     {
-#if defined(ZEUS_PLATFORM_WINDOWS)
-        namespace fs = std::filesystem;
-#else
         namespace fs = std::experimental::filesystem;
-#endif
 
         fs::path file_path{filename};
         [[maybe_unused]] std::error_code code;
@@ -33,17 +24,12 @@ namespace zeus
             fmt::print(stderr, "warning: ({}): {}\n", code.value(), code.message());
         }
 
-#if defined(ZEUS_PLATFORM_WINDOWS)
-        auto system_time = std::chrono::clock_cast<std::chrono::system_clock>(ftime);
-        return std::chrono::system_clock::to_time_t(system_time);
-#else
         return decltype(ftime)::clock::to_time_t(ftime);
-#endif
     }
 
     inline std::string get_file_directory(std::string filename)
     {
-        namespace fs = std::filesystem;
+        namespace fs = std::experimental::filesystem;
 
         fs::path file_path{filename};
         std::string root_dir;
