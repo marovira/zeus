ba75df0bd39747faf70d0702c8aef593dba6f410
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-04-07 10:43:29 -0700

    [brief] Cleaning out the string headers so I can start again.
    [detailed]
    - Attempting to add unicode support is a horrible idea, and it will lead
      down a rabbit hole that won't end well, so starting again.

--- include/zeus/string.hpp
+++ include/zeus/string.hpp
@@ -8,79 +8,79 @@
 
 namespace zeus
 {
-    template<typename T, typename U>
-    requires is_string<T>
-    std::vector<T> split(T const& str, U delim)
-    {
-        static_assert(std::is_same<typename T::value_type, U>::value);
-        std::vector<T> items;
-        T cur;
-        for (auto ch : str)
-        {
-            if (ch == delim)
-            {
-                items.push_back(cur);
-                cur.clear();
-                continue;
-            }
+    // template<typename T, typename U>
+    // requires is_string<T>
+    // std::vector<T> split(T const& str, U delim)
+    //{
+    //     static_assert(std::is_same<typename T::value_type, U>::value);
+    //     std::vector<T> items;
+    //     T cur;
+    //     for (auto ch : str)
+    //     {
+    //         if (ch == delim)
+    //         {
+    //             items.push_back(cur);
+    //             cur.clear();
+    //             continue;
+    //         }
 
-            cur.push_back(ch);
-        }
-        items.push_back(cur);
+    //        cur.push_back(ch);
+    //    }
+    //    items.push_back(cur);
 
-        return items;
-    }
+    //    return items;
+    //}
 
-    template<typename T>
-    requires is_string<T>
-    std::vector<T> split(T const& str, T const& delim)
-    {
-        if (delim.size() > str)
-        {
-            return {str};
-        }
+    // template<typename T>
+    // requires is_string<T>
+    // std::vector<T> split(T const& str, T const& delim)
+    //{
+    //     if (delim.size() > str)
+    //     {
+    //         return {str};
+    //     }
 
-        std::vector<T> items;
-        std::size_t prev_item{0};
-        for (std::size_t i{0}; i < str.size() - delim.size(); ++i)
-        {
-            auto substr = str.substr(i, delim.size());
-            if (substr == delim)
-            {
-                auto item = str.substr(prev_item, prev_item - i);
-                items.push_back(item);
-                continue;
-            }
-        }
+    //    std::vector<T> items;
+    //    std::size_t prev_item{0};
+    //    for (std::size_t i{0}; i < str.size() - delim.size(); ++i)
+    //    {
+    //        auto substr = str.substr(i, delim.size());
+    //        if (substr == delim)
+    //        {
+    //            auto item = str.substr(prev_item, prev_item - i);
+    //            items.push_back(item);
+    //            continue;
+    //        }
+    //    }
 
-        return items;
-    }
+    //    return items;
+    //}
 
-    template<typename T>
-    requires is_string<T>
-    void trim(T& str)
-    {
-        // Trim leading whitespace.
-        str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](auto ch) {
-                      return !std::isspace(ch);
-                  }));
+    // template<typename T>
+    // requires is_string<T>
+    // void trim(T& str)
+    //{
+    //     // Trim leading whitespace.
+    //     str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](auto ch) {
+    //                   return !std::isspace(ch);
+    //               }));
 
-        // Trim trailing whitespace.
-        str.erase(std::find_if(str.rbegin(),
-                               str.rend(),
-                               [](auto ch) {
-                                   return !std::isspace(ch);
-                               })
-                      .base(),
-                  str.end());
-    }
+    //    // Trim trailing whitespace.
+    //    str.erase(std::find_if(str.rbegin(),
+    //                           str.rend(),
+    //                           [](auto ch) {
+    //                               return !std::isspace(ch);
+    //                           })
+    //                  .base(),
+    //              str.end());
+    //}
 
-    template<typename T>
-    requires is_string<T>
-    T trim(T const& str)
-    {
-        T ret{str};
-        trim(ret);
-        return ret;
-    }
+    // template<typename T>
+    // requires is_string<T>
+    // T trim(T const& str)
+    //{
+    //     T ret{str};
+    //     trim(ret);
+    //     return ret;
+    // }
 } // namespace zeus
--- test/string_test.cpp
+++ test/string_test.cpp
@@ -4,118 +4,118 @@
 
 TEST_CASE("[string] - split", "[zeus]")
 {
-    const char delim = '.';
+    // const char delim = '.';
 
-    SECTION("No delimiters")
-    {
-        std::string s                     = "some random text";
-        std::vector<std::string> expected = {s};
+    // SECTION("No delimiters")
+    //{
+    //     std::string s                     = "some random text";
+    //     std::vector<std::string> expected = {s};
 
-        auto items = zeus::split(s, delim);
-        REQUIRE(items == expected);
-    }
+    //    auto items = zeus::split(s, delim);
+    //    REQUIRE(items == expected);
+    //}
 
-    SECTION("Single delimiter at beginning")
-    {
-        std::string s                     = ".some random text";
-        std::vector<std::string> expected = {"", "some random text"};
+    // SECTION("Single delimiter at beginning")
+    //{
+    //     std::string s                     = ".some random text";
+    //     std::vector<std::string> expected = {"", "some random text"};
 
-        auto items = zeus::split(s, delim);
-        REQUIRE(items == expected);
-    }
+    //    auto items = zeus::split(s, delim);
+    //    REQUIRE(items == expected);
+    //}
 
-    SECTION("Single delimiter at end")
-    {
-        std::string s                     = "some random text.";
-        std::vector<std::string> expected = {"some random text", ""};
+    // SECTION("Single delimiter at end")
+    //{
+    //     std::string s                     = "some random text.";
+    //     std::vector<std::string> expected = {"some random text", ""};
 
-        auto items = zeus::split(s, delim);
-        REQUIRE(items == expected);
-    }
+    //    auto items = zeus::split(s, delim);
+    //    REQUIRE(items == expected);
+    //}
 
-    SECTION("Single delimiter in string")
-    {
-        std::string s = "some.random text";
+    // SECTION("Single delimiter in string")
+    //{
+    //     std::string s = "some.random text";
 
-        std::vector<std::string> expected = {"some", "random text"};
+    //    std::vector<std::string> expected = {"some", "random text"};
 
-        auto items = zeus::split(s, delim);
-        REQUIRE(items == expected);
-    }
+    //    auto items = zeus::split(s, delim);
+    //    REQUIRE(items == expected);
+    //}
 
-    SECTION("Multiple delimiters")
-    {
-        std::string s                     = ".some.random.text.";
-        std::vector<std::string> expected = {"", "some", "random", "text", ""};
+    // SECTION("Multiple delimiters")
+    //{
+    //     std::string s                     = ".some.random.text.";
+    //     std::vector<std::string> expected = {"", "some", "random", "text", ""};
 
-        auto items = zeus::split(s, delim);
-        REQUIRE(items == expected);
-    }
+    //    auto items = zeus::split(s, delim);
+    //    REQUIRE(items == expected);
+    //}
 }
 
 TEST_CASE("[string] - trim", "[zeus]")
 {
-    const std::string exp{"some text"};
-    const std::string whitespace{" \f\n\r\t\v"};
+    // const std::string exp{"some text"};
+    // const std::string whitespace{" \f\n\r\t\v"};
 
-    SECTION("Non-const input")
-    {
-        SECTION("No whitespace")
-        {
-            std::string s{"some text"};
-            zeus::trim(s);
-            REQUIRE(s == exp);
-        }
+    // SECTION("Non-const input")
+    //{
+    //     SECTION("No whitespace")
+    //     {
+    //         std::string s{"some text"};
+    //         zeus::trim(s);
+    //         REQUIRE(s == exp);
+    //     }
 
-        SECTION("Leading whitespace")
-        {
-            std::string s = whitespace + exp;
+    //    SECTION("Leading whitespace")
+    //    {
+    //        std::string s = whitespace + exp;
 
-            zeus::trim(s);
-            REQUIRE(s == exp);
-        }
+    //        zeus::trim(s);
+    //        REQUIRE(s == exp);
+    //    }
 
-        SECTION("Trailing whitespace")
-        {
-            std::string s = exp + whitespace;
+    //    SECTION("Trailing whitespace")
+    //    {
+    //        std::string s = exp + whitespace;
 
-            zeus::trim(s);
-            REQUIRE(s == exp);
-        }
+    //        zeus::trim(s);
+    //        REQUIRE(s == exp);
+    //    }
 
-        SECTION("Leading and trailing whitespace")
-        {
-            std::string s = whitespace + exp + whitespace;
+    //    SECTION("Leading and trailing whitespace")
+    //    {
+    //        std::string s = whitespace + exp + whitespace;
 
-            zeus::trim(s);
-            REQUIRE(s == exp);
-        }
-    }
+    //        zeus::trim(s);
+    //        REQUIRE(s == exp);
+    //    }
+    //}
 
-    SECTION("Const input")
-    {
-        SECTION("No whitespace")
-        {
-            const std::string s{"some text"};
-            REQUIRE(zeus::trim(s) == exp);
-        }
+    // SECTION("Const input")
+    //{
+    //     SECTION("No whitespace")
+    //     {
+    //         const std::string s{"some text"};
+    //         REQUIRE(zeus::trim(s) == exp);
+    //     }
 
-        SECTION("Leading whitespace")
-        {
-            const std::string s = whitespace + exp;
-            REQUIRE(zeus::trim(s) == exp);
-        }
+    //    SECTION("Leading whitespace")
+    //    {
+    //        const std::string s = whitespace + exp;
+    //        REQUIRE(zeus::trim(s) == exp);
+    //    }
 
-        SECTION("Trailing whitespace")
-        {
-            const std::string s = exp + whitespace;
-            REQUIRE(zeus::trim(s) == exp);
-        }
+    //    SECTION("Trailing whitespace")
+    //    {
+    //        const std::string s = exp + whitespace;
+    //        REQUIRE(zeus::trim(s) == exp);
+    //    }
 
-        SECTION("Leading and trailing whitespace")
-        {
-            const std::string s = whitespace + exp + whitespace;
-            REQUIRE(zeus::trim(s) == exp);
-        }
-    }
+    //    SECTION("Leading and trailing whitespace")
+    //    {
+    //        const std::string s = whitespace + exp + whitespace;
+    //        REQUIRE(zeus::trim(s) == exp);
+    //    }
+    //}
 }
