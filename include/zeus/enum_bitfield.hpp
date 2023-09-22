9584bbf0c31742fbb8ac8c1e45cd68613634e96a
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-09-22 12:32:22 -0700

    [brief] Removes the check_flag function from the enum_bitfield header.
    [detailed]
    - The function is no longer used and was deprecated, so it should be
      removed.

--- include/zeus/enum_bitfield.hpp
+++ include/zeus/enum_bitfield.hpp
@@ -202,15 +202,4 @@ namespace zeus
             return EnumBitfield<T>{val} << n;
         }
     } // namespace enum_bitwise_operators
-
-    template<UnsignedScopedEnum T>
-    [[deprecated]]
-    inline constexpr bool check_flag(T bit_field, T flag)
-    {
-        using namespace magic_enum::bitwise_operators;
-
-        T c      = bit_field & flag;
-        auto res = magic_enum::enum_integer(c);
-        return res != 0;
-    }
 } // namespace zeus
