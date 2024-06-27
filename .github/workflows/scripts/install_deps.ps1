3d5144721b797f745751250ced170d6dcbb6dbfb
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-06-26 17:08:26 -0700

    [brief] Remove the parallel flags.
    [detailed]
    - Forgot they're used at build time, not config time.

--- .github/workflows/scripts/install_deps.ps1
+++ .github/workflows/scripts/install_deps.ps1
@@ -16,14 +16,14 @@ if(-not (Test-Path $c))
     cd ../..
 
     git clone https://github.com/fmtlib/fmt.git --branch $fmt_ver --depth 1
-    cmake -G "Visual Studio 17 2022" -S fmt -B fmt/build --parallel -DCMAKE_CXX_STANDARD=20 -DFMT_INSTALL=ON -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=C:/deps/fmt
+    cmake -G "Visual Studio 17 2022" -S fmt -B fmt/build -DCMAKE_CXX_STANDARD=20 -DFMT_INSTALL=ON -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=C:/deps/fmt
     cd ./fmt/build
     cmake --build . --config $env:BUILD_TYPE
     cmake --build . --target INSTALL --config $env:BUILD_TYPE
     cd ../..
 
     git clone https://github.com/catchorg/Catch2.git --branch v$catch_ver --depth 1
-    cmake -G "Visual Studio 17 2022" -S Catch2 -B Catch2/build --parallel -DCMAKE_CXX_STANDARD=20 -DCATCH_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=C:/deps/Catch2
+    cmake -G "Visual Studio 17 2022" -S Catch2 -B Catch2/build -DCMAKE_CXX_STANDARD=20 -DCATCH_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=C:/deps/Catch2
     cd ./Catch2/build
     cmake --build . --config $env:BUILD_TYPE
     cmake --build . --target INSTALL --config $env:BUILD_TYPE
--- .github/workflows/scripts/install_deps.sh
+++ .github/workflows/scripts/install_deps.sh
@@ -10,20 +10,20 @@ if [ ! -d ~/deps ]; then
 
     mkdir ~/deps
     git clone https://github.com/Neargye/magic_enum --branch $me_ver --depth 1
-    cmake -S magic_enum -B magic_enum/build --parallel -DCMAKE_CXX_STANDARD=20 -DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF -DMAGIC_ENUM_OPT_INSTALL=ON -DMAGIC_ENUM_OPT_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=~/deps/magic_enum
+    cmake -S magic_enum -B magic_enum/build -DCMAKE_CXX_STANDARD=20 -DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF -DMAGIC_ENUM_OPT_INSTALL=ON -DMAGIC_ENUM_OPT_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=~/deps/magic_enum
     cd ./magic_enum/build
     make
     make install
     cd ../..
 
     git clone https://github.com/fmtlib/fmt.git --branch $fmt_ver --depth 1
-    cmake -S fmt -B fmt/build --parallel -DCMAKE_CXX_STANDARD=20 -DFMT_INSTALL=ON -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=~/deps/fmt
+    cmake -S fmt -B fmt/build -DCMAKE_CXX_STANDARD=20 -DFMT_INSTALL=ON -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=~/deps/fmt
     cd ./fmt/build
     make
     make install
     cd ../..
     git clone https://github.com/catchorg/Catch2.git --branch $catch_ver --depth 1
-    cmake -S Catch2 -B Catch2/build --parallel -DCMAKE_CXX_STANDARD=20 -DCATCH_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=~/deps/Catch2
+    cmake -S Catch2 -B Catch2/build -DCMAKE_CXX_STANDARD=20 -DCATCH_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=~/deps/Catch2
     cd ./Catch2/build
     make
     make install
