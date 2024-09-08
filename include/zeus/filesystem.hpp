d790f392b8ab26abe63992bee4e9ccfc78d8ec16
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-09-08 12:33:27 -0700

    [brief] Fixes compiler issues on Apple.
    [detailed]
    - The get_file_last_write function is going to need to be disabled on
      Apple since their support of chrono is weird. It doesn't follow the
      implementation that GCC and Clang both have, but it also doesn't
      follow that MSVC does. Since this function is not really used outside
      of a few cases that can't run in Apple platforms to begin with, it's
      better off staying disabled for now.

--- include/zeus/filesystem.hpp
+++ include/zeus/filesystem.hpp
@@ -5,7 +5,7 @@
 #include <fmt/printf.h>
 #include <string>
 
-#if defined(ZEUS_PLATFORM_WINDOWS)
+#if defined(ZEUS_PLATFORM_WINDOWS) || defined(ZEUS_PLATFORM_APPLE)
 #    include <filesystem>
 #else
 #    include <experimental/filesystem>
@@ -13,33 +13,36 @@
 
 namespace zeus
 {
+#if !defined(ZEUS_PLATFORM_APPLE)
     inline std::time_t get_file_last_write(std::string filename)
     {
-#if defined(ZEUS_PLATFORM_WINDOWS)
+#    if defined(ZEUS_PLATFORM_WINDOWS)
         namespace fs = std::filesystem;
-#else
+#    else
         namespace fs = std::experimental::filesystem;
-#endif
+#    endif
 
         fs::path file_path{filename};
-        [[maybe_unused]] std::error_code code;
+        [[maybe_unused]]
+        std::error_code code;
         auto ftime = fs::last_write_time(file_path, code);
         if (code && current_build == BuildType::debug)
         {
             fmt::print(stderr, "warning: ({}): {}\n", code.value(), code.message());
         }
 
-#if defined(ZEUS_PLATFORM_WINDOWS)
+#    if defined(ZEUS_PLATFORM_WINDOWS)
         auto system_time = std::chrono::clock_cast<std::chrono::system_clock>(ftime);
         return std::chrono::system_clock::to_time_t(system_time);
-#else
+#    else
         return decltype(ftime)::clock::to_time_t(ftime);
-#endif
+#    endif
     }
+#endif
 
     inline std::string get_file_directory(std::string filename)
     {
-#if defined(ZEUS_PLATFORM_WINDOWS)
+#if defined(ZEUS_PLATFORM_WINDOWS) || defined(ZEUS_PLATFORM_APPLE)
         namespace fs = std::filesystem;
 #else
         namespace fs = std::experimental::filesystem;
