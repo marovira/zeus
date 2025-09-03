d0bb57457adc03499d74c6ade4cc248dc2da1e26
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-09-03 15:54:32 -0700

    [brief] Enable MacOS support.
    [detailed]
    - I'm dealing with Mac enough that I think it's time to allow it.

--- include/zeus/platform.hpp
+++ include/zeus/platform.hpp
@@ -10,6 +10,13 @@ namespace zeus
     {
         windows = 0,
         linux,
+        apple,
+    };
+
+    enum class ApplePlatform
+    {
+        arm64,  // Denotes Apple Silicon CPU.
+        x86_64, // Denotes Intel CPU.
     };
 
     enum class BuildType
@@ -22,7 +29,17 @@ namespace zeus
 #    define ZEUS_PLATFORM_WINDOWS
     static constexpr Platform current_platform{Platform::windows};
 #elif defined(__APPLE__)
-#    error "Zeus does not support Apple platforms"
+#    define ZEUS_PLATFORM_APPLE
+    static constexpr Platform current_platform{Platform::apple};
+#    if defined(__arm64__) || defined(__arm64)
+#        define ZEUS_PLATFORM_APPLE_ARM64
+    static constexpr ApplePlatform current_apple_platform{ApplePlatform::arm64};
+#    elif defined(__x86_64) || defined(__x86_64__)
+#        define ZEUS_PLATFORM_APPLE_X86_64
+    static constexpr ApplePlatform current_apple_platform{ApplePlatform::x86_64};
+#    else
+#        error "Unknown Apple platform detected"
+#    endif
 #elif defined(__linux__)
 #    define ZEUS_PLATFORM_LINUX
     static constexpr Platform current_platform{Platform::linux};
--- test/platform_test.cpp
+++ test/platform_test.cpp
@@ -4,17 +4,39 @@
 #include <magic_enum/magic_enum.hpp>
 
 #if defined(ZEUS_PLATFORM_WINDOWS)
-TEST_CASE("[platform] - template flags: windows", "[zeus]")
+TEST_CASE("[platform] - platform flags: windows", "[zeus]")
 {
     using zeus::current_platform;
     STATIC_REQUIRE(magic_enum::enum_name(current_platform) == "windows");
 }
+#elif defined(ZEUS_PLATFORM_APPLE)
+TEST_CASE("[platform] - platform flags: apple", "[zeus]")
+{
+    using zeus::current_platform;
+    STATIC_REQUIRE(magic_enum::enum_name(current_platform) == "apple");
+}
 #elif defined(ZEUS_PLATFORM_LINUX)
-TEST_CASE("[platform] - template flags: linux", "[zeus]")
+TEST_CASE("[platform] - platform flags: linux", "[zeus]")
 {
     using zeus::current_platform;
     STATIC_REQUIRE(magic_enum::enum_name(current_platform) == "linux");
 }
+#endif
+
+#if defined(ZEUS_PLATFORM_APPLE)
+#    if defined(ZEUS_PLATFORM_APPLE_ARM64)
+TEST_CASE("[platform] - apple flags: arm64", "[zeus]")
+{
+    using zeus::current_apple_platform;
+    STATIC_REQUIRE(magic_enum::enum_name(current_apple_platform) == "arm64");
+}
+#    elif defined(ZEUS_PLATFORM_APPLE_X86_64)
+TEST_CASE("[platform] - apple flags: x86_64", "[zeus]")
+{
+    using zeus::current_apple_platform;
+    STATIC_REQUIRE(magic_enum::enum_name(current_apple_platform) == "x86_64");
+}
+#    endif
 #endif
 
 #if defined(ZEUS_BUILD_DEBUG)
