9214e047c549be225d1f285e43cc9e9f23e0f3f6
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-11-18 16:25:28 -0800

    [brief] Use Catch's seed for RNG tests.
    [detailed]

--- test/random_test.cpp
+++ test/random_test.cpp
@@ -1,11 +1,12 @@
 #include <zeus/random.hpp>
 
+#include <catch2/catch_get_random_seed.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEMPLATE_TEST_CASE("[Random] - get_Random_in_range", "[zeus]", int, float, double)
 {
-    zeus::Random<TestType> rand;
+    zeus::Random<TestType> rand{Catch::getSeed()};
 
     auto r = rand.get_random_in_range(TestType{0}, TestType{10});
     REQUIRE(r >= TestType{0});
@@ -14,7 +15,7 @@ TEMPLATE_TEST_CASE("[Random] - get_Random_in_range", "[zeus]", int, float, doubl
 
 TEMPLATE_TEST_CASE("[Random] get_Random_zero_to_max", "[zeus]", int, float, double)
 {
-    zeus::Random<TestType> rand;
+    zeus::Random<TestType> rand{Catch::getSeed()};
 
     auto r = rand.get_random_zero_to_max();
     REQUIRE(r >= TestType{0});
@@ -22,7 +23,7 @@ TEMPLATE_TEST_CASE("[Random] get_Random_zero_to_max", "[zeus]", int, float, doub
 
 TEMPLATE_TEST_CASE("[Random] get_Random_zero_to_one", "[zeus]", int, float, double)
 {
-    zeus::Random<TestType> rand;
+    zeus::Random<TestType> rand{Catch::getSeed()};
 
     auto r = rand.get_random_zero_to_one();
     REQUIRE(r >= TestType{0});
